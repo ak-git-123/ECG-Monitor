5ab9883e01004f4c12d9a5edb6fa4e3dfcc9b408
ak-git-123 <akamath987@gmail.com>
2025-11-16 19:33:23 -0800

    Updated master_controller.py to include additional step of generating and uploading new firmware file to Gateway MCU (step2_flash_firmware.py main method).

--- python/pipeline/master_controller.py
+++ python/pipeline/master_controller.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 # === Import local modules ===
 from python.pipeline.step1_generate_dataset import main as generate_dataset_main
+from python.pipeline.step2_flash_firmware import main as generate_and_flash_firmware
 from python.pipeline.step3_batchprocess import main as batch_test_main
 from python.validation.compare_csv_data_rpeak_bpm import main as compare_data_main
 from python.pipeline.config import get_output_dir
@@ -15,11 +16,11 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 datasets_dir = os.path.join(project_root, "datasets")
 print(datasets_dir)
 
-patient_id = "P00000"
-segment_id = "s00"
-file_total = patient_id.lower() + "_" + segment_id
+patient_id = "P00001"
+segment_id = "s05"
+file_id_total = patient_id.lower() + "_" + segment_id
 file_name = os.path.join(
-    datasets_dir, f"ECG_Data_{patient_id}", segment_id, file_total
+    datasets_dir, f"ECG_Data_{patient_id}", segment_id, file_id_total
 )  # CHANGE THIS LINE TO CHANGE FILE
 
 output_dir = get_output_dir(patient_id, segment_id)
@@ -28,24 +29,29 @@ print(f"✅ Output directory: {output_dir}")
 
 
 def run_full_pipeline():
+
     print("\n🚀 Starting full ECG processing pipeline...\n")
 
     # === STEP 1: Generate dataset ===
     print("=== STEP 1: Generating ECG dataset ===")
     digital_dataset, r_peaks, num_peaks, inst_bpms = generate_dataset_main(
-        file_name=file_name, output_csv_path=output_dir, bit_res=12, start_s=0, end_s=30
+        file_name=file_name, output_csv_path=output_dir, bit_res=12, start_s=0, end_s=15
     )
     print("✅ Dataset generation complete.\n")
 
-    # === STEP 2: Run batch processing ===
-    print("=== STEP 2: Running batch processing ===")
+    print("=== STEP 2: Generating firmware file and flashing it to ESP32")
+    generate_and_flash_firmware(digital_dataset=digital_dataset, file_id=file_id_total)
+    print("✅ Firmware flashing complete.\n")
+
+    # === STEP 3: Run batch processing ===
+    print("=== STEP 3: Running batch processing ===")
     batch_test_main(
         file_name, output_csv_path=output_dir
     )  # This saves the batch processed outputs
     print("✅ Batch processing complete.\n")
 
-    # === STEP 3: Stream data from MCU ===
-    print("=== STEP 3: Running MCU streaming ===")
+    # === STEP 4: Stream data from MCU ===
+    print("=== STEP 4: Running MCU streaming ===")
     print(
         "🧠 Note: This will open the real-time ECG GUI. Close the window when done to continue."
     )
@@ -55,8 +61,8 @@ def run_full_pipeline():
     )  # add project_root to make sure it is running from project root, not working directory
     print("✅ Streaming complete.\n")
 
-    # === STEP 4: Graph and compare ===
-    print("=== STEP 4: Generating comparison graphs ===")
+    # === STEP 5: Graph and compare ===
+    print("=== STEP 5: Generating comparison graphs ===")
     compare_data_main(csv_logs_folder_path=output_dir)
     print("✅ Graph generation complete.\n")
 
